86d0563dd04e7ea66106586b7ada980daef94249
abmdeve <tundederome@gmail.com>
2024-09-21 19:22:09 +0200

    Update the code
    Refactoring code

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,8 @@
-import { Inject, Module } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule, ConfigService } from '@nestjs/config';
-import { dirname } from 'path';
 
 @Module({
   imports: [
@@ -12,7 +11,6 @@ import { dirname } from 'path';
       isGlobal: true, // Rendre la config globale
     }),
     TypeOrmModule.forRootAsync({
-      // imports: [ConfigModule],
       inject: [ConfigService], // Injecter le service de config
       useFactory: (config: ConfigService) => ({
         type: 'mysql',
@@ -26,27 +24,6 @@ import { dirname } from 'path';
       }),
     }),
   ],
-  // imports: [
-  //   useFactory: ((config: ConfigService) => (
-  //     TypeOrmModule.forRoot({
-  //       type: 'mysql',
-  //       host: process.env.DB_HOST || 'localhost',
-  //       port: +process.env.PORT_DATABASE || 3306,
-  //       username: process.env.DB_USERNAME || 'root',
-  //       // password: 'root',
-  //       password: process.env.DB_PASSWORD || '',
-  //       database: process.env.DB_NAME_DATABASE || 'mydb',
-  //       entities: [],
-  //       synchronize: true,
-  //     }),
-  //   ),
-  // Inject: [ConfigService]
-  // ),
-  //   ConfigModule.forRoot({
-  //     envFilePath: '../.env',
-  //     isGlobal: true
-  //   })
-  // ],
 
   controllers: [AppController],
   providers: [AppService],
